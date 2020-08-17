f577d2606f12fd7d565fe902e92b3694042e02d9
anita-sharma <2016uce1171@mnit.ac.in>
2020-08-17 16:46:24 +0530

    Updates

---
+++ precache-manifest.b6dbbeadc3578e08c4a3d1848ca1ee57.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65a4c918dec511b40846b7fc1c9c86a3",
+    "url": "/quizapp/index.html"
+  },
+  {
+    "revision": "66a374ea040eb6554615",
+    "url": "/quizapp/static/css/main.19674409.chunk.css"
+  },
+  {
+    "revision": "6c2a203a841251de16fe",
+    "url": "/quizapp/static/js/2.70fdcdf7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quizapp/static/js/2.70fdcdf7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66a374ea040eb6554615",
+    "url": "/quizapp/static/js/main.d912c4e5.chunk.js"
+  },
+  {
+    "revision": "4a64fee4b188f18b6a1d",
+    "url": "/quizapp/static/js/runtime-main.da13a292.js"
+  }
+]);
